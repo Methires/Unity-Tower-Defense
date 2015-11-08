490aa619fc9b4264e02a72fa4b24e63f8bf6a37d
Methires <daniel.fernes@gmail.com>
2015-11-08 22:51:19 +0100

    First steps
    Preparations

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int _startingPoint;
+    private int _endingPoint;
+    private int[,] _aValues;
+    private GameObject[,] _tiles;
+	void Start ()
+	{
+	    int length = FindObjectOfType<SpawnTiles>().Lenght;
+	    int width = FindObjectOfType<SpawnTiles>().Width;
+	    _startingPoint = Random.Range(0, length - 1);
+        _endingPoint = Random.Range(0, length - 1);
+	    _tiles = FindObjectOfType<SpawnTiles>().GetAllTiles();
+        _aValues = new int[length,width];
+	}
+	
+	void Update ()
+    {	
+	}
+}
---
+++ Assets/Scripts/SpawnTiles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SpawnTiles : MonoBehaviour
+{
+    public GameObject Tile;
+    [Header("Sizes")] 
+    public int Lenght;
+    public int Width;
+
+    private GameObject[,] _tiles;
+	void Start ()
+    {
+        _tiles = new GameObject[Lenght,Width];
+	    for (int i = 0; i < Lenght; i++)
+	    {
+	        for (int j = 0; j < Width; j++)
+	        {
+	            GameObject tile = Instantiate(Tile);
+	            tile.transform.parent = transform;
+                Vector3 position = new Vector3
+                {
+                    x = i * 1.05f,
+                    y = 0.0f,
+                    z = j * 1.05f
+                };
+	            tile.transform.localPosition = position;
+	            _tiles[i, j] = tile;
+	        }
+	    }
+    }
+
+    public GameObject[,] GetAllTiles()
+    {
+        return _tiles;
+    }
+}
