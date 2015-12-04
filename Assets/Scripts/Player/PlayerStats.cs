33b57250e32ef939d2535a983ca9ef89a055ffa6
Methires <daniel.fernes@gmail.com>
2015-12-04 01:32:00 +0100

    Initial connection of TD with FPS

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("GUI Bar Textures")]
+    public Texture2D HpTexture2D;
+    public Texture2D StaminaTexture2D;
+    [Header("Statistics")]
+    public float MaxHealth;
+    public float MaxStamina;
+    
+    private float _currentHealth = 100;
+    private float _currentStamina = 100;
+    private float _barWidth;
+    private float _barHeight;
+    private bool _canRegenerateStamina;
+    private float _staminaRegenarationDelay;
+    private float _DelayValue = 2.0f;
+    private Vector3 _lastPosition;
+    private CharacterController _characterController;
+    private FirstPersonController _firstPersonController;
+
+    void Awake()
+    {
+        _barHeight = Screen.height * 0.04f;
+        _barWidth = _barHeight * 10.0f;
+        _characterController = FindObjectOfType<CharacterController>().GetComponent<CharacterController>();
+        _firstPersonController = FindObjectOfType<FirstPersonController>().GetComponent<FirstPersonController>();
+        _lastPosition = transform.position;
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(Screen.width - _barWidth - 10,
+                                 Screen.height - _barHeight - 10,
+                                 _currentHealth * _barWidth / MaxHealth,
+                                 _barHeight), 
+                                 HpTexture2D);
+        GUI.DrawTexture(new Rect(Screen.width - _barWidth - 10,
+                                 Screen.height - _barHeight * 2 - 20,
+                                 _currentStamina * _barWidth / MaxStamina,
+                                 _barHeight),
+                                 StaminaTexture2D);
+    }
+
+    void FixedUpdate()
+    {
+        if (_characterController.isGrounded && Input.GetKey(KeyCode.LeftShift) && _lastPosition != transform.position && _currentStamina > 0)
+        {
+            _lastPosition = transform.position;
+            _currentStamina -= 0.75f;
+            _currentStamina = Mathf.Clamp(_currentStamina, 0, MaxStamina);
+            _canRegenerateStamina = false;
+        }
+        else
+        {
+            _canRegenerateStamina = true;
+        }
+
+        if (_canRegenerateStamina)
+        {
+            _staminaRegenarationDelay += 0.5f * Time.deltaTime;
+            if (_staminaRegenarationDelay >= _DelayValue)
+            {
+                _staminaRegenarationDelay = _DelayValue;
+                if (Math.Abs(_currentStamina - MaxStamina) > 0.1f)
+                {
+                    _currentStamina += 1.0f;
+                    _currentStamina = Mathf.Clamp(_currentStamina, 0, MaxStamina);
+                }
+                else
+                {
+                    _canRegenerateStamina = false;
+                    _staminaRegenarationDelay = 0.0f;
+                }
+            }
+        }
+
+        _firstPersonController.CanRun = _currentStamina > 0;
+    }
+
+    void DecreaseHealth(float damage)
+    {
+        _currentHealth -= damage;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, MaxHealth);
+        if (_currentHealth < 0)
+        {
+            //Game Over
+        }
+    }
+
+    void IncreaseHealth(float value)
+    {
+        _currentHealth += value;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0, MaxHealth);
+    }
+
+}
---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Shooting : MonoBehaviour
+{
+    public Texture2D CrosshairTexture;
+    [Header("Sounds")]
+    public AudioClip ShotSound;
+    public AudioClip ReloadSound;
+    public AudioClip NoAmmoSound;
+    [Header("Weapon Params")] 
+    public int AttackValue;
+    public int MaxAmmo;
+    public int ClipSize;
+    public float Range;
+    public float ReloadTime;
+
+    private int _currentAmmo;
+    private int _currentClip;
+    private bool _isReloading;
+    private float _reloadTimeCounter;
+    private Rect _crosshairPosition;
+
+	void Start ()
+	{
+        _isReloading = false;
+        _reloadTimeCounter = 0.0f;
+	    Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _crosshairPosition = new Rect((Screen.width - CrosshairTexture.width) / 2 , 
+                             (Screen.height - CrosshairTexture.height) / 2,
+                             CrosshairTexture.width, 
+                             CrosshairTexture.height);
+	    _currentClip = ClipSize;
+	    _currentAmmo = MaxAmmo - ClipSize;
+       // GetComponent<GUIText>().text = "Clip: " + _currentClip + " | Ammo: " + _currentAmmo;
+	}
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(_crosshairPosition, CrosshairTexture);
+    }
+
+    void Update () 
+    {
+	    if (Input.GetKeyDown(KeyCode.Escape))
+	    {
+	        Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+	    }
+
+	    if (Input.GetButtonDown("Fire1"))
+	    {
+	        Shoot();
+	    }
+
+        if (Input.GetKeyDown(KeyCode.R) && _currentClip != ClipSize)
+        {
+            Reload();
+        }
+
+        if (_isReloading)
+        {
+            _reloadTimeCounter += Time.deltaTime * 1.1f;
+            if (_reloadTimeCounter > ReloadTime)
+            {
+                _isReloading = false;
+                _reloadTimeCounter = 0.0f;
+                //GetComponent<GUIText>().text = "Clip: " + _currentClip + " | Ammo: " + _currentAmmo;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (_currentClip > 0 && !_isReloading)
+        {
+            GetComponent<AudioSource>().clip = ShotSound;
+            _currentClip--;
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, forward, out hit))
+            {
+                if (hit.transform.gameObject.tag == "Enemy" && hit.distance < Range)
+                {
+                    hit.transform.gameObject.GetComponent<EnemyBehaviour>().ReceiveDamage(AttackValue);
+                    Debug.Log("You hit enemy");
+                }
+                else if (hit.distance > Range && hit.transform.gameObject == null)
+                {
+                    Debug.Log("You hit nothing");
+                }
+            }
+            GetComponent<AudioSource>().Play();
+            //GetComponent<GUIText>().text = "Clip: " + _currentClip + " | Ammo: " + _currentAmmo;
+        }
+        else if (_currentClip == 0)
+        {
+            Reload();
+        }
+    }
+
+    private void Reload()
+    {
+        GetComponent<AudioSource>().clip = ReloadSound;
+        if (_currentAmmo > 0 && !_isReloading)
+        {
+            _isReloading = true;
+            if (_currentAmmo >= ClipSize)
+            {
+                _currentAmmo -= ClipSize - _currentClip;
+                _currentClip = ClipSize;
+            }
+            else
+            {
+                _currentClip = _currentAmmo;
+                _currentAmmo = 0;
+            }
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            GetComponent<AudioSource>().clip = NoAmmoSound;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    public void RenewAmmo()
+    {
+        _currentAmmo = MaxAmmo;
+        //GetComponent<GUIText>().text = "Clip: " + _currentClip + " | Ammo: " + _currentAmmo;
+    }
+}
